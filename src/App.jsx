364f8c8a6a34679c2fcf0277002cdc6f9f6a89ac
RizkiHibatuloh <rizkihibatuloh0123@gmail.com>
2024-11-09 01:45:58 +0700

    fetch data dari api untuk mengisi fungsi format data

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,32 @@ const API_URL = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_K
 
 function App() {
 
+  const formatApiData = (apiResult) => {
+    const result = {
+      curr: {
+        title: "Currency",
+        values:[],
+      },
+      purchaseRate: {
+        title: "We Buy",
+        values:[],
+      },
+      exchangeRate: {
+        title: "Excange Rate",
+        values:[],
+      },
+      sellRate: {
+        title: "We Sell",
+        values:[],
+      },
+    };
+
+    for(const key in apiResult.rates) {
+      result.curr.values.push(key);
+      result.exchangeRate.values.push(apiResult.rates[key]);
+      
+    }
+  }
   const fetchCurrencyData = async () => {
     try{
       const res = await fetch(API_URL);
@@ -15,7 +41,7 @@ function App() {
         throw response;
       }
       const result = await res.json();
-      console.log(result);
+      formatApiData(result);
       
     } 
     catch (error) {
@@ -30,18 +56,14 @@ useEffect (() => {
     <>
       <main className='flex min-h-screen bg-orange-500 justify-center items-center'>
         <section className='flex w-2/4 justify-around text-center items-center'>
+        
           <div className="w-1/2">
             <h1 className="mb-2.5 text-white">{"Currency"}</h1>
             <div>
               <p className="mb-1.5 text-white">1000</p>
             </div>
           </div>
-          <div className="w-1/2">
-            <h1 className="mb-2.5 text-white">{"Currency"}</h1>
-            <div>
-              <p className="mb-1.5 text-white">1000</p>
-            </div>
-          </div>
+        
         </section>
       </main>
 
