17a39afb9fd4e6715a1c3880f0f456874c47fa8f
RizkiHibatuloh <rizkihibatuloh0123@gmail.com>
2024-11-09 02:41:36 +0700

    call data untuk di tampilkan serta membuat filter currencies yang ingin di tampilkan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useEffect, useState } from 'react';
 
-const API_KEY = "a78b827a01b8419c8a4bb4d5cc3219ae";
+const API_KEY = "dc826d39132d4d95a932981db3dc8a70";
 const API_URL = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}`;
 
 
 function App() {
+  const [ currencies, setCurrencies ] = useState([])
 
+  const getPercentageValue = (numStr, percentage) => {
+    const num = parseFloat(numStr);
+
+    return (num * percentage) / 100;
+  };
+  
+  const getPurchaseRate = (exchangeRate, percentage) => {
+    return parseFloat(exchangeRate) + percentage;
+  };
+
+  const getSellRate = (exchangeRate, percentage) => {
+    return parseFloat(exchangeRate) - percentage;
+  };
+
+    //Data fetching
   const formatApiData = (apiResult) => {
+    const allowedCurrencies = ["CAD", "EUR", "IDR", "JPY", "CHF", "GBP"]; // filter untuk memilih mata uang yang ditampilkan
     const result = {
       curr: {
         title: "Currency",
@@ -27,12 +44,33 @@ function App() {
     };
 
     for(const key in apiResult.rates) {
-      result.curr.values.push(key);
+      if(allowedCurrencies.includes(key)) {
+        result.curr.values.push(key);
       result.exchangeRate.values.push(apiResult.rates[key]);
-      
+
+      const percentage = getPercentageValue(apiResult.rates[key],5);
+      const purchaseRate = getPurchaseRate(apiResult.rates[key],percentage);
+      const sellRate = getSellRate(apiResult.rates[key],percentage);
+
+      result.purchaseRate.values.push(purchaseRate);
+      result.sellRate.values.push(sellRate);
+      }
     }
+    setCurrencies(result);
   }
-  const fetchCurrencyData = async () => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const fetchCurrencyData = async () => {     //fetching API
+    
+    // caching data 
+    const cachedData = localStorage.getItem("currencyData");
+    const lastFetched = localStorage.getItem("lastFetched");
+    const currentTime = new Date().getTime();
+
+    if (cachedData && lastFetched && currentTime - lastFetched < 86400000) {
+      setCurrencies(JSON.parse(cachedData));
+      return;
+    }
+    
     try{
       const res = await fetch(API_URL);
 
@@ -51,19 +89,31 @@ function App() {
  
 useEffect (() => {
   fetchCurrencyData();
-}, []);
+  
+}, [fetchCurrencyData]);
   return (
     <>
       <main className='flex min-h-screen bg-orange-500 justify-center items-center'>
         <section className='flex w-2/4 justify-around text-center items-center'>
-        
-          <div className="w-1/2">
-            <h1 className="mb-2.5 text-white">{"Currency"}</h1>
-            <div>
-              <p className="mb-1.5 text-white">1000</p>
-            </div>
-          </div>
-        
+        {
+          Object.keys(currencies).map((section, sectionIndex) => {    //looping data dari object APi untuk di tampilkan
+            return(
+              <div className="w-1/2" key={sectionIndex}>
+                <h1 className="mb-2.5 text-white">
+                  {currencies[section].title}
+                </h1>
+                {currencies[section].values.map((currValue, currValueindex) => {
+                  return(
+                    <div key={`${currValue}-${currValueindex}`}>
+                        <p className='mb-1.5 text-white'>{currValue}</p>
+                    </div>
+                  )
+                })}
+
+              </div>
+            )
+          })
+        }
         </section>
       </main>
 
